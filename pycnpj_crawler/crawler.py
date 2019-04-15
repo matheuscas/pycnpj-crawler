f794429995922fff8382a3355521dcb50b6aae0d
Matheus <matheus.mcas@gmail.com>
2019-04-14 23:29:38 -0300

    First implementation of get_cnpj_data

--- pycnpj_crawler/crawler.py
+++ pycnpj_crawler/crawler.py
@@ -1,3 +1,17 @@
+from pycnpj_crawler.states.util import load_state_class
+
 
 def get_cnpj_data(cnpj, state="ba"):
-    return None
+    """
+    Busca no site do estado os dados do CNPJ provido
+
+    Args:
+        cnpj (str): CNPJ, somente numeros, da empresa alvo
+        state (str): Sigla do estado, em letras minusculas
+
+    Returns:
+        cnpj (obj)
+    """
+
+    state = load_state_class(state)
+    return state().get_cnpj_data(cnpj)
