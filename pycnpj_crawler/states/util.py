7b61f7417f1c9c09c76bc9229e6dc5bdb598aab8
Matheus <matheus.mcas@gmail.com>
2019-04-14 23:29:07 -0300

    Add util module to hold useful functions related to states

---
+++ pycnpj_crawler/states/util.py
@@ -0,0 +1,14 @@
+BA = "ba"
+
+_states_mapping = {}
+_states_mapping[BA] = "Bahia"
+
+
+def get_state_name(state):
+    return _states_mapping[state]
+
+
+def load_state_class(state):
+    module = __import__(f"{state}", globals(), locals(), [], 1)
+    state_name = get_state_name(state)
+    return getattr(module, state_name)
